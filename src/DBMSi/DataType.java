8a0ddf38958b022f15c5344cd54ec91571569610
clupspv <claudia.peiro.2015@salleurl.edu>
2017-06-22 11:16:27 +0200

    Initial commit

---
+++ src/DBMSi/DataType.java
@@ -0,0 +1,22 @@
+package DBMSi;
+
+/**
+ * Created by Albertpv on 15/01/17.
+ *
+ *
+ * <p>Possibles valors que poden tenir les columnes
+ * de qualsevol taula del sistema.</p>
+ *
+ * @author Programació Avançada i Estructura de Dades (PAED)
+ *         Universitat La Salle Ramon Llull
+ */
+public enum DataType {
+
+    BOOLEAN,
+    INT,
+    LONG,
+    FLOAT,
+    DOUBLE,
+    CHAR,
+    TEXT
+}
